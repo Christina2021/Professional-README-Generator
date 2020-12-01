c4a1e6998c67fdc4558ad916488df43894fcaebe
Christina2021 <65867599+Christina2021@users.noreply.github.com>
2020-12-01 14:39:39 -0800

    Added questions to questions array. Updated functions so that user is given prompts for questions/responses are used to create README by using function from generateMarkdown.js file.

--- index.js
+++ index.js
@@ -1,39 +1,92 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
-/*
-//Tested inquirer to confirm linked (will delete when beginning code)
-inquirer
-    .prompt([
-        {
-            type: 'input',
-            message: 'Is this working?',
-            name: 'worked',
-        }
-    ])
-    .then((response) => {
-        console.log(`${response.worked}`)
-
-        //Tested fs to confirm linked
-        fs.writeFile('text.txt','test', (err) => {
-            err ? console.error(err) : console.log('html worked.')
-        });
-    });
-*/
 
 //array of questions for user
 const questions = [
-
+    'What is the name of your project?',
+    'Please enter in a description of your project:',
+    'Please enter in the command that should be used to install the dependencies:',
+    'Please enter in the command that should be used to run tests:',
+    'Please enter usage information for the user:',
+    'Please enter information on how a user may contribute to the project:',
+    'Please choose a license for your project:',
+    'What is your GitHub username?',
+    'What is your email address?'
 ];
 
 //function to write README file
 function writeToFile(fileName, data) {
+    
+    fs.writeFile(fileName,generateMarkdown(data), (err) => {
+        if(err) console.error(`There was an error: ${err}`);
+    });
 
 };
 
 //function to initialize program
 function init() {
+    inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: questions[0],
+            name: 'title',
+        },
+        {
+            type: 'input',
+            message: questions[1],
+            name: 'description',
+        },
+        {
+            type: 'input',
+            message: questions[2],
+            name: 'install',
+        },
+        {
+            type: 'input',
+            message: questions[3],
+            name: 'test',
+        },
+        {
+            type: 'input',
+            message: questions[4],
+            name: 'usage',
+        },
+        {
+            type: 'input',
+            message: questions[5],
+            name: 'contribution',
+        },
+        {
+            type: 'list',
+            message: questions[6],
+            name: 'license',
+            choices: [
+                "MIT",
+                "APACHE-2.0",
+                "GPL-3.0",
+                "BSD-3",
+                "No License",
+            ]
+        },
+        {
+            type: 'input',
+            message: questions[7],
+            name: 'username',
+        },
+        {
+            type: 'input',
+            message: questions[8],
+            name: 'email',
+        },
+    ])
+    .then((response) => {
 
+        writeToFile('New-README.md',response);
+
+    });
 }
 
 //function call to initialize program
